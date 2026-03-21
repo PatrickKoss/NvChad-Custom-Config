9162f56b9f8785a102fb5a7e0aebd43c9bd18657
Patrick Koss <pati.koss@gmx.de>
2026-03-21 23:04:49 +0100

    remove copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -199,54 +199,6 @@ return {
     end,
   },
 
-  -- Copilot
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        panel = {
-          enabled = true,
-          auto_refresh = true,
-          keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>",
-          },
-          layout = {
-            position = "bottom",
-            ratio = 0.4,
-          },
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<Tab>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        },
-        filetypes = {
-          yaml = false,
-          markdown = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          ["."] = false,
-        },
-        copilot_node_command = "node",
-      })
-    end,
-  },
-
   -- Autoclose
   {
     "m4xshen/autoclose.nvim",
